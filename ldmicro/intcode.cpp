e085859db741bb30b8d6167e27a8a0e6c46c2639
Sven Schlender <svesch@gmx.de>
2012-11-06 16:40:04 +0100

    Forgot end if for new write string instruction.
    --HG--
    branch : Netzer

--- ldmicro/intcode.cpp
+++ ldmicro/intcode.cpp
@@ -669,6 +669,7 @@ static void IntCodeFromCircuit(int which, void *any, char *stateInOut)
         case ELEM_STRING: {
             Op(INT_IF_BIT_SET, stateInOut);
             Op(INT_WRITE_STRING, l->d.move.src, l->d.move.dest);
+            Op(INT_END_IF);
             break;
         }
 
